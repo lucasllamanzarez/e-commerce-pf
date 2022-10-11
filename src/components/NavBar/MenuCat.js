4b9ce53aa26a1f66e66ff6f9b8c7d77a149fb15c
lucasllamanzarez <103078227+lucasllamanzarez@users.noreply.github.com>
2022-10-11 10:20:48 -0300

    Update MenuCat.js

--- src/components/NavBar/MenuCat.js
+++ src/components/NavBar/MenuCat.js
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
-import {useState} from 'react'
+import {useState} from 'react';
 import './Navbar.css';
 
 const MenuCat = () => {
